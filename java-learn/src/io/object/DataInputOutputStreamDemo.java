5083d32011696fca61008dd2019fe7013e16021c
KunUpUpUp <826580243@qq.com>
2025-07-13 13:06:47 +0800

    update

---
+++ java-learn/src/io/object/DataInputOutputStreamDemo.java
@@ -0,0 +1,41 @@
+package io.object;
+
+import java.io.*;
+
+public class DataInputOutputStreamDemo {
+    public static void main(String[] args) throws IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("D:\\test.txt"));
+
+        DataOutputStream os = new DataOutputStream(new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+                System.out.write(b); // 将字节输出到控制台
+            }
+
+            @Override
+            public void close() throws IOException {
+                System.out.close(); // 关闭输出流
+            }
+
+            @Override
+            public void flush() throws IOException {
+                System.out.flush(); // 刷新输出流
+            }
+        });
+
+        try {
+            os.writeUTF("Hello World"); // 写入UTF字符串
+//            os.writeInt(123); // 写入整数
+//            os.writeBoolean(true); // 写入布尔值
+            os.flush(); // 刷新输出流
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                os.close(); // 关闭输出流
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- spring/src/main/java/com/seasugar/controller/StudentController.java
+++ spring/src/main/java/com/seasugar/controller/StudentController.java
@@ -1,7 +1,9 @@
 package com.seasugar.controller;
 
+import com.seasugar.domain.Student;
 import com.seasugar.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
     @Autowired
     private StudentService studentService;
+
+    @GetMapping("/get")
+    public Student get(){
+        return studentService.getById(1L);
+    }
 }
